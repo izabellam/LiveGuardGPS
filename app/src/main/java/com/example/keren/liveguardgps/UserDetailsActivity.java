31a4b1520b313503f25f956582cadbd177124619
izabellam <35331301+izabellam@users.noreply.github.com>
2018-01-11 12:52:45 +0200

    change dp

--- app/src/main/java/com/example/keren/liveguardgps/UserDetailsActivity.java
+++ app/src/main/java/com/example/keren/liveguardgps/UserDetailsActivity.java
@@ -20,16 +20,16 @@ public class UserDetailsActivity extends Activity {
         setContentView(R.layout.user_details);
 
 
-        final EditText user_pswd = (EditText) findViewById(R.id.user_pswd);
-        final EditText user_email = (EditText)findViewById(R.id.user_email);
+        final EditText user_login_pswd = (EditText) findViewById(R.id.user_login_pswd);
+        final EditText user_login_email = (EditText)findViewById(R.id.user_login_email);
 
 
         Button save_user_details = (Button) findViewById(R.id.save_user_details);
         save_user_details.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String userpswd = user_pswd.getText().toString();
-                String user_email_str = user_email.getText().toString();
+                String userpswd = user_login_pswd.getText().toString();
+                String user_email_str = user_login_email.getText().toString();
 
                 // go to database save user
 
